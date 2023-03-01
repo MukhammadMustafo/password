10a7227a9fc6d717727fd20dba1f9f54954d1cdf
MukhammadMustafo <mukhammad20000@gmail.com>
2023-03-01 15:19:04 +0500

    123

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  const pass = (e)=>{
+    e.preventDefault()
+
+  }
+  const long = (e)=>{
+    e.preventDefault()
+    console.log(e.target.value);
+    if(e.target.value.length >= 8){
+      e.target.style.border = '3px solid green'
+    }else if (e.target.value.length > 12){
+      e.target.style.border = '3px solid red'
+    }else{
+      e.target.style.border = '3px solid red'
+    }
+
+  }
+  return (
+    <div className="App">
+      <h1>WELCOME</h1>
+      <form action="#" onSubmit={pass}>
+        <input type="text" name='pass1' onChange={long}/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
